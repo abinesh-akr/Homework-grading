cda2797bfb2c04d463702226691cec7f014ac385
abinesh-akr <12345sabinesh_bcs26@mepcoeng.ac.in>
2025-03-17 11:47:38 +0530

    Update Spellings.py

--- Spellings/Spellings.py
+++ Spellings/Spellings.py
@@ -1,48 +1,39 @@
-import enchant
+from textblob import TextBlob
 import re
 
 ########## function which checks spellings ##############
-# 		INPUT : essay
-# 		OUTPUT: returns the number of misspelt words AND key-value pairs of missplet words and suggestions to correct them
+#         INPUT : essay
+#         OUTPUT: returns the number of misspelt words AND key-value pairs of misspelled words and suggestions to correct them
 
 def spellCheck(text):
-	#choose the dictionary
-	d = enchant.Dict("en_US")
-	
-	#keep track of the number of misspelt words
-	numIncorrect=0
-	
-	#split the text into words
-	wordList = re.findall(r'\w+', text)
-	
-	misspelt = {}
-	
-	### Checking for misspelt words ###
-	for word in wordList:
-		#print word, d.check(word)
-		if d.check(word)==False:
-			misspelt[word] = d.suggest(word)	#store the word and its suggestions as a key value pair
-			numIncorrect += 1
-			
-	return numIncorrect, misspelt
-
-
-
+    # Split the text into words
+    wordList = re.findall(r'\w+', text)
+    
+    misspelt = {}
+    numIncorrect = 0
+    
+    ### Checking for misspelled words ###
+    for word in wordList:
+        blob = TextBlob(word)
+        corrected_word = str(blob.correct())
+        
+        if corrected_word != word:
+            misspelt[word] = corrected_word  # Store the word and its suggestion
+            numIncorrect += 1
+            
+    return numIncorrect, misspelt
 
 ######  INDEPENDENTLY TESTING THE SCRIPT ########
-if __name__ == '__main__' :
-	
-	#### open essay, in the format (.txt) ####
-	sourceFileName = "Sample_Essays/technology.txt"
-	sourceFile = open(sourceFileName, "r")
-	
-	#read essay
-	text = sourceFile.read()
-	
-	incorr2, misspelt2 = spellCheck(text)
-	print ("\n\nIncorrectly spelled count: ", incorr2, "\n")
-	
-	for key in misspelt2:
-		print (key, " :: ",  misspelt2[key], "\n\n")
-	
-	
+if __name__ == '__main__':
+    
+    #### open essay, in the format (.txt) ####
+    sourceFileName = "Sample_Essays/technology.txt"
+    with open(sourceFileName, "r", encoding="utf-8") as sourceFile:
+        # Read essay
+        text = sourceFile.read()
+    
+    incorr2, misspelt2 = spellCheck(text)
+    print("\n\nIncorrectly spelled count: ", incorr2, "\n")
+    
+    for key in misspelt2:
+        print(key, " :: ", misspelt2[key], "\n\n")
